7b155992f8cbbb5db16dee792a108d71d17961a5
Matthew Rooke <matthew.rooke@my.jcu.edu.au>
2019-09-09 14:31:29 +1000

    Remove old test and Add new tests for each Class.

---
+++ src/TrafficLightTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TrafficLightTest {
+    Road road = new Road("0", 1, 5, new int[]{0, 0});
+    TrafficLight light = new TrafficLight("0", road);
+
+    @Test
+    void getState() {
+        assertEquals("green", light.getState());
+    }
+
+    @Test
+    void getRoad() {
+        assertEquals(road, light.getRoad());
+    }
+
+    @Test
+    void getPosition() {
+        assertEquals(5, light.getPosition());
+    }
+
+    @Test
+    void getId() {
+        assertEquals("light_0", light.getId());
+    }
+}
