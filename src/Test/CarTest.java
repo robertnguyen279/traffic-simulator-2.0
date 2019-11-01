8a79cb39f4aca03029be31b29ea5d9de189a2268
Matt Rooke <matthew.rooke@my.jcu.edu.au>
2019-11-01 22:29:10 +1000

    Update Tests

--- src/Test/CarTest.java
+++ src/Test/CarTest.java
@@ -1,45 +1,46 @@
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class CarTest {
-//    Model.Road road = new Model.Road("0", 1, 5, new int[]{0, 0});
-//    Model.Car car = new Model.Car("0", road);
-//
-//    @Test
-//    void testMove() {
-//        car.move();
-//        assertEquals(2, car.getPosition());
-//    }
-//
-//    @Test
-//    void getLength() {
-//        assertEquals(1, car.getLength());
-//    }
-//
-//    @Test
-//    void getBreadth() {
-//        assertEquals(0.5, car.getBreadth());
-//    }
-//
-//    @Test
-//    void getSpeed() {
-//        assertEquals(0, car.getSpeed());
-//    }
-//
-//    @Test
-//    void getPosition() {
-//        assertEquals(1, car.getPosition());
-//    }
-//
-//    @Test
-//    void getRoad() {
-//        assertEquals(road, car.getCurrentRoad());
-//    }
-//
-//    @Test
-//    void getId() {
-//        assertEquals("car_0", car.getId());
-//    }
-//
-//}
+import Model.Road;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CarTest {
+    Model.Road road = new Model.Road("0", 1, 5, new int[]{0, 0}, Road.Orientation.VERTICAL);
+    Model.Car car = new Model.Car("0", road);
+
+    @Test
+    void testMove() {
+        car.move();
+        assertEquals(-3, car.getPosition());
+    }
+
+    @Test
+    void getLength() {
+        assertEquals(4, car.getLength());
+    }
+
+    @Test
+    void getBreadth() {
+        assertEquals(2.0, car.getBreadth());
+    }
+
+    @Test
+    void getSpeed() {
+        assertEquals(0, car.getSpeed());
+    }
+
+    @Test
+    void getPosition() {
+        assertEquals(-4, car.getPosition());
+    }
+
+    @Test
+    void getRoad() {
+        assertEquals(road, car.getCurrentRoad());
+    }
+
+    @Test
+    void getId() {
+        assertEquals("car_0", car.getId());
+    }
+
+}
--- src/Test/MotorbikeTest.java
+++ src/Test/MotorbikeTest.java
@@ -1,23 +1,24 @@
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class MotorbikeTest {
-//    Model.Motorbike bike = new Model.Motorbike("0", new Model.Road());
-//
-//    @Test
-//    void getLength() {
-//        assertEquals(0.5, bike.getLength());
-//    }
-//
-//    @Test
-//    void getId() {
-//        assertEquals("bike_0", bike.getId());
-//    }
-//
-//    @Test
-//    void testInheritance() {
-//        assertEquals(0, bike.getSpeed());
-//        assertEquals(1, bike.getPosition());
-//    }
-//}
+import Model.Road;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MotorbikeTest {
+    Model.Motorbike bike = new Model.Motorbike("0", new Model.Road("0", 1, 5, new int[]{0, 0}, Road.Orientation.VERTICAL));
+
+    @Test
+    void getLength() {
+        assertEquals(2.0, bike.getLength());
+    }
+
+    @Test
+    void getId() {
+        assertEquals("bike_0", bike.getId());
+    }
+
+    @Test
+    void testInheritance() {
+        assertEquals(0, bike.getSpeed());
+        assertEquals(-2, bike.getPosition());
+    }
+}
